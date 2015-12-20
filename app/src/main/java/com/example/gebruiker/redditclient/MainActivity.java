e281d5ba78fefccc5568092dec249c2f34e0bf55
Jan Van Braeckel <jan.vanbraeckel.x7253@student.hogent.be>
2015-12-20 16:03:41 +0100

    Small change

--- app/src/main/java/com/example/gebruiker/redditclient/MainActivity.java
+++ app/src/main/java/com/example/gebruiker/redditclient/MainActivity.java
@@ -1,10 +1,7 @@
 package com.example.gebruiker.redditclient;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.widget.SwipeRefreshLayout;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -14,8 +11,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
-
-import com.example.gebruiker.redditclient.service.RedditService;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
